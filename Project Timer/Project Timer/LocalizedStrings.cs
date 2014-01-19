6d2277cdf68bdd0a4b7879fd0731bed1846691f8
basvandenheuvel <bassie-boy@live.nl>
2014-01-19 13:45:20 +0100

    Leeg project aangemaakt
    Nieuw leeg project aangemaakt.

---
+++ Project Timer/Project Timer/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Project_Timer.Resources;
+
+namespace Project_Timer
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
