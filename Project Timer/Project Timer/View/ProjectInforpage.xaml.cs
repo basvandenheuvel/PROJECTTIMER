2b1272f5b9a5475ad11cbe61437fd6655a337994
basvandenheuvel <bassie-boy@live.nl>
2014-01-23 16:41:27 +0100

    ProjectInfo page gemaakt. NOG NIET GEBIND
    OOk op task en worktime page een info knop toegevoegd

---
+++ Project Timer/Project Timer/View/ProjectInforpage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Project_Timer.View
+{
+    public partial class ProjectInforpage : PhoneApplicationPage
+    {
+        public ProjectInforpage()
+        {
+            InitializeComponent();
+        }
+    }
+}
